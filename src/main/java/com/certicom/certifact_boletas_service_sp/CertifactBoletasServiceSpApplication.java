5a9f856c4b6999c452133fb280fb9cb8d0b10ba1
Jose Zambrano <21zam03.free@gmail.com>
2025-09-06 14:57:43 -0500

    new beginning

---
+++ src/main/java/com/certicom/certifact_boletas_service_sp/CertifactBoletasServiceSpApplication.java
@@ -0,0 +1,13 @@
+package com.certicom.certifact_boletas_service_sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertifactBoletasServiceSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertifactBoletasServiceSpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/certicom/certifact_boletas_service_sp/CertifactBoletasServiceSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certicom.certifact_boletas_service_sp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertifactBoletasServiceSpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
