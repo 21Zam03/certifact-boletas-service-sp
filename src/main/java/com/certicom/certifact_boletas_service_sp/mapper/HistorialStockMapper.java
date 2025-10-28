13ab2e3acfcc10ec2fa9d3531834d2f13cb8e455
Jose Zambrano <21zam03.free@gmail.com>
2025-10-28 10:21:42 -0500

    new update

--- src/main/java/com/certicom/certifact_boletas_service_sp/mapper/HistorialStockMapper.java
+++ src/main/java/com/certicom/certifact_boletas_service_sp/mapper/HistorialStockMapper.java
@@ -1,7 +1,9 @@
 package com.certicom.certifact_boletas_service_sp.mapper;
 
 import com.certicom.certifact_boletas_service_sp.model.HistorialStockModel;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface HistorialStockMapper extends BaseMapper<HistorialStockModel, Long> {
 
     int deleteByDetailsPaymentVoucher(Long idDetailsPayment);
